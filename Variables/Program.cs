5c17deb6c2769e1f46c4125744179c479e082aa2
Gavin Stark <gavin@gstark.com>
2021-04-13 11:25:26 -0400

    Adds a Console.WriteLine to ask the user for their name and use Console.Readline to get their answer

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -7,8 +7,19 @@ namespace Variables
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to C#");
+            // Output a blank line
+            Console.WriteLine("");
+            // This too outputs a blank line
+            Console.WriteLine();
+            Console.WriteLine("This is on another line");
 
-            var name = "Samantha";
+            Console.Write("What is your name? ");
+
+            // This "HARD CODES" the value for name
+            // var name = "Samantha";
+
+            // Instead, lets read the value for the name variable from the console!
+            var name = Console.ReadLine();
 
             var score = 42;
 
@@ -29,10 +40,16 @@ namespace Variables
             var sentence = "The quick brown fox jumped over the lazy dog";
             var howLong = sentence.Length;
 
+            // Increment the score so that we have the latest
             score = score + 1;
 
+            // Hitting the goal doubles our points
             score = score * 2;
 
+            // Fouling causes our score to be cut in fourths
+            // more comments here
+            // Continue more comments here
+            // Even more comments
             score = score / 4;
 
             score = score - 10;
