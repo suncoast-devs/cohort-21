e978bc82655cd96e096a801dcbb9573233d92ca5
Gavin Stark <gavin@gstark.com>
2021-04-13 10:28:16 -0400

    Other variable types (bool), char

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -15,6 +15,21 @@ namespace Variables
             var school = "SDG";
 
             var numberOfStudents = 11;
+
+            var price = 5000000000;
+
+            var letter = 'x';
+
+            var letterButDigit = '1';
+
+            var cohort21Started = true;
+
+            var lectureEnded = false;
+
+            var sentence = "The quick brown fox jumped over the lazy dog";
+            var howLong = sentence.Length;
+
+
         }
     }
 }
