ab6dee22ce5a33f932e91eff3f03a0326d01f16e
Gavin Stark <gavin@gstark.com>
2021-04-14 11:24:47 -0400

    Example of a while loop

--- ControlFlow/Program.cs
+++ ControlFlow/Program.cs
@@ -6,6 +6,20 @@ namespace ControlFlow
     {
         static void Main(string[] args)
         {
+
+            Console.Write("What is your name? [type quit to stop]? ");
+            var name = Console.ReadLine();
+
+            while (name != "quit")
+            {
+                Console.WriteLine($"Nice to see you {name}");
+
+                Console.Write("What is your name? [type quit to stop]? ");
+                name = Console.ReadLine();
+            }
+
+            Console.WriteLine("-----------------");
+
             Console.Write("What is your name? ");
             var userName = Console.ReadLine();
 
@@ -18,8 +32,9 @@ namespace ControlFlow
                 Console.WriteLine($"It is nice to meet you, {userName}!");
             }
 
+            Console.WriteLine("This is the next line of code");
 
-            if (userName == "Paul")
+            if (userName == "Paul" || userName == "Peter")
             {
                 Console.WriteLine("Here");
             }
@@ -34,6 +49,27 @@ namespace ControlFlow
             else
             {
                 Console.WriteLine("Didn't find anything");
+            }
+
+            switch (userName)
+            {
+                case "Paul":
+                case "Peter":
+                    Console.WriteLine("Here");
+                    break;
+                case "Dorothy":
+                    Console.WriteLine("Also Here");
+                    break;
+                case "Sam":
+                    Console.WriteLine("Here Again");
+                    break;
+                case "Gavin":
+                    Console.WriteLine("Hi there Gavin!");
+                    Console.WriteLine("More lines of code");
+                    break;
+                default:
+                    Console.WriteLine("Didn't find anything");
+                    break;
             }
 
         }
