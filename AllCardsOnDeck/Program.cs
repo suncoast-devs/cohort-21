7d67e85034011f2bb0e19a10cc24edc3b8b11e22
Gavin Stark <gavin@gstark.com>
2021-04-15 09:16:32 -0400

    Algorithm for generating a deck of cards

--- AllCardsOnDeck/Program.cs
+++ AllCardsOnDeck/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AllCardsOnDeck
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            var deck = new List<string>();
+
+            var suits = new List<string>() { "Clubs", "Diamonds", "Hearts", "Spades" };
+            var ranks = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+
+            // Go through each suit, Clubs, Diamonds, Hearts, and Spaces, one at a time
+            //     For each suit do the following
+            //        Go through all the ranks, Ace, 2, 3, 4, etc.
+            //           For each combination of suit and rank, make a new string
+            //           add that string to our deck of cards list
+        }
     }
-  }
 }
