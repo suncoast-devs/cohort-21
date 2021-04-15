f7be26c40bba2b9ae2b9a0a1ad46872979b097f5
Gavin Stark <gavin@gstark.com>
2021-04-15 10:07:11 -0400

    Adds a display greeting METHOD

--- EmployeeDatabaseWithMethods/Program.cs
+++ EmployeeDatabaseWithMethods/Program.cs
@@ -4,13 +4,35 @@ namespace EmployeeDatabaseWithMethods
 {
     class Program
     {
-        static void Main(string[] args)
+        // This is where our new method goes!
+
+        // static method (ignore this for the moment)
+        //  |
+        //  |  The return (output) type. Here there is none
+        //  |  since the method isn't giving anything back
+        //  |  to the code that called it.
+        //  |   |
+        //  |   |               The inputs, known as arguments. None in this case.
+        //  |   |                   |
+        //  |   |                   |
+        //  v   v                   v
+        static void DisplayGreeting()
         {
+            //
+            //    Body of the method
+            //      |
+            //      |
+            //      v
             Console.WriteLine("--------------------------------");
             Console.WriteLine("Welcome to Our Employee Database");
             Console.WriteLine("--------------------------------");
             Console.WriteLine();
             Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            DisplayGreeting();
 
             Console.Write("What is your name? ");
             var name = Console.ReadLine();
