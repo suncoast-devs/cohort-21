f3dac17e4e1210fbc9e9d68a713456245abd4c06
Gavin Stark <gavin@gstark.com>
2021-04-15 09:49:52 -0400

    Simple code to get information about an employee

--- EmployeeDatabaseWithMethods/Program.cs
+++ EmployeeDatabaseWithMethods/Program.cs
@@ -2,11 +2,28 @@
 
 namespace EmployeeDatabaseWithMethods
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Welcome to Our Employee Database");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.Write("What is your name? ");
+            var name = Console.ReadLine();
+
+            Console.Write("What is your department number? ");
+            var department = int.Parse(Console.ReadLine());
+
+            Console.Write("What is your yearly salary (in dollars)? ");
+            var salary = int.Parse(Console.ReadLine());
+
+            var salaryPerMonth = salary / 12;
+            Console.WriteLine($"Hello, {name} you make {salaryPerMonth} a month.");
+        }
     }
-  }
+
 }
