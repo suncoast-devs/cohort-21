4e1ac913c124147997f8da0435f0f58ff74fd65a
Gavin Stark <gavin@gstark.com>
2021-04-15 11:31:30 -0400

    Simpler style for declaring a class that has properties (uses the getter/setter style)

--- EmployeeDatabaseWithClasses/Program.cs
+++ EmployeeDatabaseWithClasses/Program.cs
@@ -2,7 +2,52 @@
 
 namespace EmployeeDatabaseWithMethods
 {
-    // This is where new classes go! 
+    // // This is where new classes go! 
+    // //
+    // // class keyword
+    // // |
+    // // |    Name of class (PascalCase)
+    // // |    |
+    // // v    v
+    // class Employee
+    // {
+    //     // public means "this can be seen outside of the class
+    //     // |
+    //     // |   Type
+    //     // |   |
+    //     // |   |      Name of property
+    //     // |   |      |
+    //     // |   |      |
+    //     // |   |      |
+    //     // v   v      v
+    //     public string Name;
+    //     public int Department;
+    //     public int Salary;
+    //     public int MonthlySalary;
+
+    //     // This is a *special* method known as a "constructor"
+    //     // The constructor is called when we write a line like: `var bob = new Employee(`
+    //     // The arguments to the method should line up to those below
+    //     //
+    //     //              This will become the employee's name
+    //     //              |               This will become the employee's department
+    //     //              |               |                  This will become the employee's salary
+    //     //              |               |                  |              This will become the employee's monthly salary
+    //     //              |               |                  |              |
+    //     //              v               v                  v              v
+    //     public Employee(string newName, int newDepartment, int newSalary, int newMonthlySalary)
+    //     {
+    //         // In the constructor we should setup the values for any of the properties.
+    //         // Here we will *copy* the values given by the arguments to the corresponding property.
+
+    //         Name = newName;
+    //         Department = newDepartment;
+    //         Salary = newSalary;
+    //         MonthlySalary = newSalary;
+    //     }
+    // }
+
+
     //
     // class keyword
     // |
@@ -17,36 +62,14 @@ namespace EmployeeDatabaseWithMethods
         // |   |
         // |   |      Name of property
         // |   |      |
-        // |   |      |
-        // |   |      |
-        // v   v      v
-        public string Name;
-        public int Department;
-        public int Salary;
-        public int MonthlySalary;
-
-        // This is a *special* method known as a "constructor"
-        // The constructor is called when we write a line like: `var bob = new Employee(`
-        // The arguments to the method should line up to those below
-        //
-        //              This will become the employee's name
-        //              |               This will become the employee's department
-        //              |               |                  This will become the employee's salary
-        //              |               |                  |              This will become the employee's monthly salary
-        //              |               |                  |              |
-        //              v               v                  v              v
-        public Employee(string newName, int newDepartment, int newSalary, int newMonthlySalary)
-        {
-            // In the constructor we should setup the values for any of the properties.
-            // Here we will *copy* the values given by the arguments to the corresponding property.
-
-            Name = newName;
-            Department = newDepartment;
-            Salary = newSalary;
-            MonthlySalary = newSalary;
-        }
+        // |   |      |      We can get the data and set the data
+        // |   |      |      |
+        // v   v      v      v
+        public string Name { get; set; }
+        public int Department { get; set; }
+        public int Salary { get; set; }
+        public int MonthlySalary { get; set; }
     }
-
 
 
     class Program
@@ -133,9 +156,21 @@ namespace EmployeeDatabaseWithMethods
             // Console.WriteLine($"Hello, {name} you make {salaryPerMonth} a month.");
 
 
-            var graceHopper = new Employee("Grace Hopper", 100, 240000, 20000);
+            var graceHopper = new Employee()
+            {
+                Department = 100,
+                Salary = 240000,
+                MonthlySalary = 20000,
+                Name = "Grace Hopper",
+            };
 
-            var elonMusk = new Employee("Elon Musk", 42, 120000, 10000);
+            var elonMusk = new Employee()
+            {
+                Salary = 120000,
+                Name = "Elon Musk",
+                Department = 42,
+                MonthlySalary = 10000,
+            };
 
             Console.WriteLine(graceHopper.Department);
             Console.WriteLine(elonMusk.Department);
