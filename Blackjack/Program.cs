00c9b1de30a1fb785cc19914790ddc5beada3f5b
Gavin Stark <gavin@gstark.com>
2021-04-19 09:33:12 -0400

    Initial questions

--- Blackjack/Program.cs
+++ Blackjack/Program.cs
@@ -2,11 +2,11 @@
 
 namespace Blackjack
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C#");
+        }
     }
-  }
 }
--- Blackjack/README.md
+++ Blackjack/README.md
@@ -1,1 +1,15 @@
 # Blackjack
+
+Questions
+
+Explorer:
+
+- Syntax
+- Too many things at once / PEDAC
+- Creating and using the card object
+- Loops / Control Flow (booleans?)
+- Methods
+
+Adventure:
+
+- Aces counting as 11 or 1
