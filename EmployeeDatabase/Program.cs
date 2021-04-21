98d8e9a7310b23dbd5e3bb877a4502f9b7b50b53
Gavin Stark <gavin@gstark.com>
2021-04-21 09:43:01 -0400

    Initial Employee class and code

--- EmployeeDatabase/Program.cs
+++ EmployeeDatabase/Program.cs
@@ -2,11 +2,63 @@
 
 namespace EmployeeDatabase
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Employee
     {
-      Console.WriteLine("Welcome to C#");
+        public string Name { get; set; }
+        public int Department { get; set; }
+        public int Salary { get; set; }
+
+        public int MonthlySalary()
+        {
+            return Salary / 12;
+        }
     }
-  }
+
+    class Program
+    {
+        static void DisplayGreeting()
+        {
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("    Welcome to Our Employee Database    ");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        static string PromptForString(string prompt)
+        {
+            Console.Write(prompt);
+            var userInput = Console.ReadLine();
+            return userInput;
+        }
+
+        static int PromptForInteger(string prompt)
+        {
+            Console.Write(prompt);
+            int userInput;
+            var isThisGoodInput = Int32.TryParse(Console.ReadLine(), out userInput);
+            if (isThisGoodInput)
+            {
+                return userInput;
+            }
+            else
+            {
+                Console.WriteLine("Sorry, that isn't a valid input, I'm using 0 as your answer.");
+                return 0;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var employee = new Employee();
+
+            DisplayGreeting();
+
+            employee.Name = PromptForString("What is your name? ");
+            employee.Department = PromptForInteger("What is your department number? ");
+            employee.Salary = PromptForInteger("What is your yearly salary (in dollars)? ");
+
+            Console.WriteLine($"Hello, {employee.Name} you make {employee.MonthlySalary()} dollars per month.");
+        }
+    }
 }
