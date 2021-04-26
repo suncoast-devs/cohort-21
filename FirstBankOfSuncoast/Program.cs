54dd4d5418e8f9499b9b399376b44635e7d183cb
Gavin Stark <gavin@gstark.com>
2021-04-26 09:18:40 -0400

    PEDAC and initial menu system

--- FirstBankOfSuncoast/Program.cs
+++ FirstBankOfSuncoast/Program.cs
@@ -2,11 +2,65 @@
 
 namespace FirstBankOfSuncoast
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static string PromptForString(string prompt)
+        {
+            Console.Write(prompt);
+
+            var userInput = Console.ReadLine();
+
+            return userInput;
+        }
+
+        static int PromptForInteger(string prompt)
+        {
+            Console.Write(prompt);
+
+            var userInput = Console.ReadLine();
+
+            var userInputAsNumber = int.Parse(userInput);
+
+            return userInputAsNumber;
+        }
+
+        static void Main(string[] args)
+        {
+            var userHasChosenToQuit = false;
+            while (userHasChosenToQuit == false)
+            {
+                Console.WriteLine("Menu Options:");
+                Console.WriteLine();
+                Console.WriteLine("Deposit");
+                Console.WriteLine("Withdraw");
+                Console.WriteLine("Transfer");
+                Console.WriteLine("Balance");
+                Console.WriteLine("History");
+                Console.WriteLine("Quit");
+
+                var choice = PromptForString("Choice: ").ToUpper().Trim();
+                switch (choice)
+                {
+                    case "DEPOSIT":
+                        break;
+
+                    case "WITHDRAW":
+                        break;
+
+                    case "TRANSFER":
+                        break;
+
+                    case "BALANCE":
+                        break;
+
+                    case "HISTORY":
+                        break;
+
+                    case "QUIT":
+                        userHasChosenToQuit = true;
+                        break;
+                }
+            }
+        }
     }
-  }
 }
