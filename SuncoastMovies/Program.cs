d5d792b89bdccaee6936fc39a8fb38b72df08c48
Gavin Stark <gavin@gstark.com>
2021-04-29 10:17:54 -0400

    Defines our Database Context (our bridge between our two worlds)

--- SuncoastMovies/Program.cs
+++ SuncoastMovies/Program.cs
@@ -1,7 +1,18 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace SuncoastMovies
 {
+    class SuncoastMoviesContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("server=localhost;database=SuncoastMovies");
+        }
+    }
+
     class Movie
     {
         public int Id { get; set; }
