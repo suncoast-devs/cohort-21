bc6282377eb468ac6aacaa0563f78cd5b59de210
Gavin Stark <gavin@gstark.com>
2021-04-29 10:27:13 -0400

    Using LINQ + our context (entity framework) to do our first query of the database (counting movies)

--- SuncoastMovies/Program.cs
+++ SuncoastMovies/Program.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 
 namespace SuncoastMovies
 {
     class SuncoastMoviesContext : DbContext
     {
+        // "Movies" is our table over in the database world
+        // "Movie" is our C# class in .NET world
+        // "DbSet" is like database powered `List`
         public DbSet<Movie> Movies { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -26,7 +30,15 @@ namespace SuncoastMovies
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to C#");
+            // Get a new context that will connect to the database
+            var context = new SuncoastMoviesContext();
+
+            // Not too different from code we've seen before
+            //
+            // var transactionCount = transactions.Count();
+            var moviesCount = context.Movies.Count();
+
+            Console.WriteLine($"There are {moviesCount} movies in our database");
         }
     }
 }
