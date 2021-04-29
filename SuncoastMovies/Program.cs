16878afe5f26c91582ed2ec7cb27c4813717e55c
Gavin Stark <gavin@gstark.com>
2021-04-29 10:04:38 -0400

    Defined a class Movie to match the definition of our Movies table

--- SuncoastMovies/Program.cs
+++ SuncoastMovies/Program.cs
@@ -2,11 +2,20 @@
 
 namespace SuncoastMovies
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Movie
     {
-      Console.WriteLine("Welcome to C#");
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string PrimaryDirector { get; set; }
+        public int YearReleased { get; set; }
+        public string Genre { get; set; }
     }
-  }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C#");
+        }
+    }
 }
