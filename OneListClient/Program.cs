1f2c0877f4c16ae6ba5f594f1fbcd8a1d3fbfcb9
Gavin Stark <gavin@gstark.com>
2021-05-04 11:36:29 -0400

    Customize the mapping of keys in JSON to PascalCaseProperty in C#

--- OneListClient/Program.cs
+++ OneListClient/Program.cs
@@ -2,17 +2,38 @@
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace OneListClient
 {
     class Item
     {
-        public int id { get; set; }
-        public string text { get; set; }
-        public bool complete { get; set; }
-        public DateTime created_at { get; set; }
-        public DateTime updated_at { get; set; }
+        // Dear serializer, please use the field "id" for this
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        // Dear serializer, please use the field "text" for this
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+
+        [JsonPropertyName("complete")]
+        public bool Complete { get; set; }
+
+        [JsonPropertyName("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonPropertyName("updated_at")]
+        public DateTime UpdatedAt { get; set; }
+
+        public string CompletedStatus
+        {
+            get
+            {
+                // return      CONDITION    ?       VALUE WHEN TRUE           :       VALUE WHEN FALSE      
+                return Complete ? "completed" : "not completed";
+            }
+        }
     }
 
     class Program
@@ -29,7 +50,7 @@ namespace OneListClient
 
             foreach (var item in items)
             {
-                Console.WriteLine($"The task {item.text} was created on {item.created_at} and has a completion of: {item.complete}");
+                Console.WriteLine($"The task {item.Text} was created on {item.CreatedAt} and has a completion of: {item.CompletedStatus}");
             }
 
         }
