12ad382023c7bb634c0cb8afb70fcebc3e894d67
Gavin Stark <gavin@gstark.com>
2021-05-04 11:17:15 -0400

    Uses JsonSerializer Deserialze to turn our JSON from the API into a List of Item objects

--- OneListClient/Program.cs
+++ OneListClient/Program.cs
@@ -1,20 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace OneListClient
 {
+    class Item
+    {
+        public int id { get; set; }
+        public string text { get; set; }
+        public bool complete { get; set; }
+        public string created_at { get; set; }
+        public string updated_at { get; set; }
+    }
+
     class Program
     {
         static async Task Main(string[] args)
         {
             var client = new HttpClient();
 
-            var responseAsString = await client.GetStringAsync(
+            var responseAsStream = await client.GetStreamAsync(
               "https://one-list-api.herokuapp.com/items?access_token=sdg-handbook"
             );
 
-            Console.WriteLine(responseAsString);
+            var items = await JsonSerializer.DeserializeAsync<List<Item>>(responseAsStream);
+
+            foreach (var item in items)
+            {
+                Console.WriteLine($"The task {item.text} was created on {item.created_at} and has a completion of: {item.complete}");
+            }
+
         }
     }
 }
