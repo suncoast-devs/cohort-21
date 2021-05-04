50061b0ec855ce042b7d1474100bc53836c6a2cf
Gavin Stark <gavin@gstark.com>
2021-05-04 11:05:15 -0400

    Prints out the JSON data result

--- OneListClient/Program.cs
+++ OneListClient/Program.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace OneListClient
 {
     class Program
     {
-        static async System.Threading.Tasks.Task Main(string[] args)
+        static async Task Main(string[] args)
         {
             var client = new HttpClient();
 
             var responseAsString = await client.GetStringAsync(
               "https://one-list-api.herokuapp.com/items?access_token=sdg-handbook"
             );
+
+            Console.WriteLine(responseAsString);
         }
     }
 }
