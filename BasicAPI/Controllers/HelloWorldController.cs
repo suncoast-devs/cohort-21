98a4a2a58cbdbbe468d0d4f52375d6e7f3ca4043
Gavin Stark <gavin@gstark.com>
2021-05-05 10:29:02 -0400

    A very simple bit of code to respond to /api/HelloWorld

---
+++ BasicAPI/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public string SayHello()
+        {
+            return "Hello, World. I am an API.";
+        }
+    }
+}
