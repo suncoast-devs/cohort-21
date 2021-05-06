7cb28881a63a48509c4ed2f5eb0f3eb3d05ba4f3
Gavin Stark <gavin@gstark.com>
2021-05-06 10:00:23 -0400

    Added our model for GameNight **FIRST** (no CREATE TABLEs for us)

---
+++ GameNightWithFriends/Models/GameNight.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace GameNightWithFriends.Models
+{
+    public class GameNight
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Host { get; set; }
+
+        public string Address { get; set; }
+
+        public DateTime When { get; set; }
+
+        public int MinimumPlayers { get; set; }
+
+        public int MaximumPlayers { get; set; }
+    }
+}
