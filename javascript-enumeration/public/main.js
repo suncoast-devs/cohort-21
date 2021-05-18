9ec8b20f7940185ce4e861a508e2c113698ab10d
Gavin Stark <gavin@gstark.com>
2021-05-18 09:56:16 -0400

    Uses forEach to transform one array into another

--- javascript-enumeration/public/main.js
+++ javascript-enumeration/public/main.js
@@ -1,15 +1,32 @@
 function main() {
   const colors = ['red', 'green', 'blue']
 
-  function logSomeColor(color, index) {
-    console.log(`The color at position ${index} is ${color}`)
-  }
+  // function logSomeColor(color, index) {
+  //   console.log(`The color at position ${index} is ${color}`)
+  // }
 
   //
   // foreach(var color in colors) {
   //
   // }
-  colors.forEach(logSomeColor)
+
+  // anonymous INLINE function
+  //
+  // anonymous since it has no function name
+  // INLINE because we declare it and use it in one step
+  colors.forEach(function (color, index) {
+    console.log(`The color at position ${index} is ${color}`)
+  })
+
+  const lengths = []
+
+  colors.forEach(function (color) {
+    const lengthOfColor = color.length
+
+    lengths.push(lengthOfColor)
+  })
+
+  console.log(lengths)
 }
 
 document.addEventListener('DOMContentLoaded', main)
