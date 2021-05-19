7ef96c15a393e4a393a6c1f097072a5603224071
Gavin Stark <gavin@gstark.com>
2021-05-19 10:22:53 -0400

    Uses addEventListener to teach the browser to call handleClickSquare every time that li is clicked

--- javascript-document-object-model/public/main.js
+++ javascript-document-object-model/public/main.js
@@ -1,3 +1,7 @@
+function handleClickSquare() {
+  window.alert('You did it!')
+}
+
 function main() {
   // Give us the very first `li` you can find.
   // Put a *REFERENCE* to that object (from the DOM)
@@ -10,6 +14,18 @@ function main() {
   // Ensure the `taken` class is applied
   // so that the cursor styling is present
   firstListItem.classList.add('taken')
+
+  //
+  // What object
+  //     |
+  //     |        please listen for an event
+  //     |            |
+  //     |            |          name of event to wait for
+  //     |            |             |
+  //     |            |             |     function to call when it happens
+  //     |            |             |        |
+  //     v            v             v        v
+  firstListItem.addEventListener('click', handleClickSquare)
 }
 
 document.addEventListener('DOMContentLoaded', main)
