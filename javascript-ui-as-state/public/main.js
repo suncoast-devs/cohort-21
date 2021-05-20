89393d0c523d64a6b1d69a7befb5c59d6998795f
Gavin Stark <gavin@gstark.com>
2021-05-20 10:05:46 -0400

    Using a "render" method to draw the entire interface and event listeners to CHANGE DATA AND CALL RENDER

--- javascript-ui-as-state/public/main.js
+++ javascript-ui-as-state/public/main.js
@@ -14,7 +14,7 @@ function render() {
     <h2>${teamOneName}</h2>
     <h3>${teamOneScore}</h3>
     <fieldset>
-      <input type="text" placeholder="Name" />
+      <input type="text" placeholder="Name" value="${teamOneName}" />
     </fieldset>
 
     <fieldset>
@@ -27,7 +27,7 @@ function render() {
     <h2>${teamTwoName}</h2>
     <h3>${teamTwoScore}</h3>
     <fieldset>
-      <input type="text" placeholder="Name" />
+      <input type="text" placeholder="Name" value="${teamTwoName}" />
     </fieldset>
 
     <fieldset>
@@ -39,6 +39,44 @@ function render() {
   `
 
   document.body.innerHTML = html
+
+  document
+    .querySelector('.team1 .add')
+    .addEventListener('click', function (event) {
+      teamOneScore++
+      render()
+    })
+  document
+    .querySelector('.team1 .subtract')
+    .addEventListener('click', function (event) {
+      teamOneScore--
+      render()
+    })
+  document
+    .querySelector('.team1 input')
+    .addEventListener('input', function (event) {
+      teamOneName = event.target.value
+      render()
+    })
+
+  document
+    .querySelector('.team2 .add')
+    .addEventListener('click', function (event) {
+      teamTwoScore++
+      render()
+    })
+  document
+    .querySelector('.team2 .subtract')
+    .addEventListener('click', function (event) {
+      teamTwoScore--
+      render()
+    })
+  document
+    .querySelector('.team2 input')
+    .addEventListener('input', function (event) {
+      teamTwoName = event.target.value
+      render()
+    })
 }
 
 function main() {
