c00b26c6e4168d4b7f7e4f609298d7bf8562ebd5
Gavin Stark <gavin@gstark.com>
2021-05-20 09:53:26 -0400

    Initial ScoreBoard HTML + CSS

--- javascript-ui-as-state/public/main.js
+++ javascript-ui-as-state/public/main.js
@@ -1,24 +1,5 @@
-let counter = 42
-
-function render() {
-  const html = `
-  <p>${counter}</p>
-  <button>Increment</button>
-  `
-
-  document.body.innerHTML = html
-}
-
 function main() {
-  render()
-
-  const button = document.querySelector('button')
-
-  button.addEventListener('click', function (event) {
-    counter = counter + 1
-
-    render()
-  })
+  // Code here
 }
 
 document.addEventListener('DOMContentLoaded', main)
