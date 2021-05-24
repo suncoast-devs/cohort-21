fa042dd1a7ae98fa9d76af6457c5ec60fc805c15
Gavin Stark <gavin@gstark.com>
2021-05-24 11:19:51 -0400

    Creates our main APP component

--- react-articles/src/App.jsx
+++ react-articles/src/App.jsx
@@ -3,20 +3,20 @@ import React, { Component } from 'react'
 export class App extends Component {
   render() {
     return (
-      <div class="all-main-content">
+      <div className="all-main-content">
         <main>
-          <article class="intro-article">
+          <article className="intro-article">
             <h2 className="article-title">SDG Announces Hackathon!</h2>
             <p>
               SDG announces the 2020 Summer Hackathon. Join us for an exciting
               weekend
             </p>
-            <a class="read-more" href="#here">
+            <a className="read-more" href="#here">
               read more about SDG Announces Hackathon!
             </a>
           </article>
 
-          <article class="intro-article">
+          <article className="intro-article">
             <h2 className="article-title">
               Student Graduation is Right Around the Corner
             </h2>
@@ -24,7 +24,7 @@ export class App extends Component {
               Our next cohort of students will be graduating in just over a
               week.
             </p>
-            <a class="read-more" href="#here">
+            <a className="read-more" href="#here">
               read more about Student Graduation is Right Around the Corner
             </a>
           </article>
