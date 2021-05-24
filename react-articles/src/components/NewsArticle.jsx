b240d9ab8ac868e956942f9ee5ed8412fea3cc52
Gavin Stark <gavin@gstark.com>
2021-05-24 11:40:47 -0400

    Uses "props" to customize my NewsArticle component

--- react-articles/src/components/NewsArticle.jsx
+++ react-articles/src/components/NewsArticle.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export class NewsArticle extends React.Component {
   render() {
-    return <div>Something</div>
+    return (
+      <article className="intro-article">
+        <h2 className="article-title">{this.props.title}</h2>
+        <p>{this.props.body}</p>
+        <a className="read-more" href="#here">
+          read more about {this.props.title}!
+        </a>
+      </article>
+    )
   }
 }
