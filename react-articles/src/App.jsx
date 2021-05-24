6e6b5008ae180c6f5dc2a6589e7843d1018d6a19
Gavin Stark <gavin@gstark.com>
2021-05-24 11:14:21 -0400

    Added our HTML (really JSX) as the return of our App components

--- react-articles/src/App.jsx
+++ react-articles/src/App.jsx
@@ -2,6 +2,34 @@ import React, { Component } from 'react'
 
 export class App extends Component {
   render() {
-    return <div>Hello, World!</div>
+    return (
+      <div class="all-main-content">
+        <main>
+          <article class="intro-article">
+            <h2 className="article-title">SDG Announces Hackathon!</h2>
+            <p>
+              SDG announces the 2020 Summer Hackathon. Join us for an exciting
+              weekend
+            </p>
+            <a class="read-more" href="#here">
+              read more about SDG Announces Hackathon!
+            </a>
+          </article>
+
+          <article class="intro-article">
+            <h2 className="article-title">
+              Student Graduation is Right Around the Corner
+            </h2>
+            <p>
+              Our next cohort of students will be graduating in just over a
+              week.
+            </p>
+            <a class="read-more" href="#here">
+              read more about Student Graduation is Right Around the Corner
+            </a>
+          </article>
+        </main>
+      </div>
+    )
   }
 }
