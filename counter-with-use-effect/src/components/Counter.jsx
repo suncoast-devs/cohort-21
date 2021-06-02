067c065ecd8973a4dcefbd64b9e1ebbb3854671b
Gavin Stark <gavin@gstark.com>
2021-06-02 10:28:17 -0400

    UseEffect with an empty "dependency array" runs only once!

--- counter-with-use-effect/src/components/Counter.jsx
+++ counter-with-use-effect/src/components/Counter.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
 
 export function Counter() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(42)
+  const [isHappy, setIsHappy] = useState(true)
 
   function handleClickButton() {
     setCount(count + 1)
@@ -11,14 +12,39 @@ export function Counter() {
     setCount(count * 2)
   }
 
+  function handleClickHappy() {
+    // if (isHappy) {
+    //   setIsHappy(false)
+    // } else {
+    //   setIsHappy(true)
+    // }
+    setIsHappy(!isHappy)
+  }
+
+  useEffect(
+    // Function to call ONCE *and* when a variable listed below
+    // changes
+    function () {
+      console.log(`This runs once when the component first mounts`)
+    },
+
+    // EXCEPT, the list here is empty, so nothing can ever change!
+    // thus our code there runs EXACTLY ONCE when the component first mounts
+    []
+  )
+
   // Using useEffect
   useEffect(
     // The function to call when a variable changes
     function () {
-      console.log(`Wow, the count changed and is now ${count}`)
+      console.log(
+        `Wow, the count changed and is now ${count} - and I am ${
+          isHappy ? 'happy' : 'not happy'
+        }`
+      )
     },
     // List of values (variables) to monitor
-    [count]
+    [count, isHappy]
   )
 
   return (
@@ -28,6 +54,8 @@ export function Counter() {
         <button onClick={handleClickButton}>Click Me!!!</button>
         <button onClick={handleOtherClickButton}>Or Click Me!!!</button>
       </p>
+      <p>Am I happy: {isHappy ? 'Yes' : 'No'}</p>
+      <button onClick={handleClickHappy}>Toggle Happiness</button>
     </div>
   )
 }
