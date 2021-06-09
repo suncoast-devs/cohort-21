c48d5ada624d85ef4cf0e48f4272d49cfc955d18
Gavin Stark <gavin@gstark.com>
2021-06-09 11:12:19 -0400

    Change default hardcoded text to use an empty state that is the same shape as the API
    next up: fetch!

--- starwars-thing/src/components/Film.jsx
+++ starwars-thing/src/components/Film.jsx
@@ -1,22 +1,31 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export function Film() {
+  const [film, setFilm] = useState({
+    title: '',
+    episode_id: 0,
+    opening_crawl: '',
+    director: '',
+    producer: '',
+    release_date: '',
+    characters: [],
+    planets: [],
+    starships: [],
+    vehicles: [],
+    species: [],
+    created: '',
+    edited: '',
+    url: '',
+  })
+
   return (
     <>
-      <h2>Episode IV: A New Hope</h2>
+      <h2>{film.title}</h2>
 
       <p className="preamble">Long long ago, in a galaxy far, far away.</p>
 
-      <div className="crawl">
-        It is a period of civil war. Rebel spaceships, striking from a hidden
-        base, have won their first victory against the evil Galactic Empire.
-        During the battle, Rebel spies managed to steal secret\r plans to the
-        Empire's ultimate weapon, the DEATH STAR, an armored space station with
-        enough power to destroy an entire planet. Pursued by the Empire's
-        sinister agents, Princess Leia races home aboard her starship, custodian
-        of the stolen plans that can save her people and restore freedom to the
-        galaxy....
-      </div>
+      <div className="crawl">{film.opening_crawl}</div>
 
       <ul className="people-list">
         <li>
