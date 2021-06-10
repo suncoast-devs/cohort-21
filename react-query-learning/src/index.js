faa2182ac38f6cf76d7f97b62558ec899ee488a1
Gavin Stark <gavin@gstark.com>
2021-06-10 10:25:50 -0400

    Added the QueryClientProvider, whatever that is... Seems to be needed

--- react-query-learning/src/index.js
+++ react-query-learning/src/index.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.scss'
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+
 import { App } from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import './index.scss'
+
+const queryClient = new QueryClient()
+
+ReactDOM.render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
+  document.getElementById('root')
+)
