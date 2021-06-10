156ab4c7c7527d1afdb4ffe4804d9140b10733e0
Gavin Stark <gavin@gstark.com>
2021-06-10 10:33:16 -0400

    Can load some basic film data from the API

--- react-query-learning/src/App.jsx
+++ react-query-learning/src/App.jsx
@@ -1,5 +1,22 @@
 import React from 'react'
+import { useQuery } from 'react-query'
 
 export function App() {
-  return <div>Hello, World!</div>
+  const { isLoading, error, data } = useQuery('filmData', () =>
+    fetch('https://swapi.dev/api/films/').then((res) => res.json())
+  )
+
+  console.log({ isLoading, error, data })
+
+  if (isLoading) return 'Loading...'
+
+  if (error) return 'An error has occurred: ' + error.message
+
+  return (
+    <div>
+      {data.results.map((film) => (
+        <p>{film.title}</p>
+      ))}
+    </div>
+  )
 }
