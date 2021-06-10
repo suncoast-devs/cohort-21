e9149763ad69b0eb9c3a518a9782c4d6e7534e9b
Gavin Stark <gavin@gstark.com>
2021-06-10 11:12:18 -0400

    Remove unused imports! We will be tidy developers!

--- starwars-thing/src/components/Home.jsx
+++ starwars-thing/src/components/Home.jsx
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import { useEffect, useState } from 'react'
 import { useQuery } from 'react-query'
 import { Link } from 'react-router-dom'
 import { getIdFromURL } from './getIdFromURL'
