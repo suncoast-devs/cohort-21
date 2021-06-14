2b954e305494da2d11ded2a5d0a64d1dbda1be0c
Gavin Stark <gavin@gstark.com>
2021-06-14 10:24:52 -0400

    Updated our JWT_KEY and made sure our React code is working/updating

--- TacoTuesday/ClientApp/src/App.jsx
+++ TacoTuesday/ClientApp/src/App.jsx
@@ -3,6 +3,8 @@ import './custom.scss'
 
 export function App() {
   return (
-    <div className="d-flex justify-content-center display-2">Hello, World!</div>
+    <div className="d-flex justify-content-center display-2">
+      Hello, World From the front end!
+    </div>
   )
 }
