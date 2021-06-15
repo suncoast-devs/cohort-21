4a8ad72c33f35139454f391b3a88b5119be83aaa
Gavin Stark <gavin@gstark.com>
2021-06-15 11:46:22 -0400

    Cleaning up code to remove an unsued import that snuck in somehow

--- TacoTuesday/ClientApp/src/pages/NewRestaurant.jsx
+++ TacoTuesday/ClientApp/src/pages/NewRestaurant.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { Link, useHistory } from 'react-router-dom'
 import avatar from '../images/avatar.png'
-import { Restaurants } from './Restaurants'
 
 export function NewRestaurant() {
   const [newRestaurant, setNewRestaurant] = useState(
