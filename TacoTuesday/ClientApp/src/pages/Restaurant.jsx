6c3a941c0075b925ab435e1425041ee728561afc
Gavin Stark <gavin@gstark.com>
2021-06-16 11:13:33 -0400

    Adds user interface to create reviews, automatically reset the review form, and reload the restaurant and all of its reviews.

--- TacoTuesday/ClientApp/src/pages/Restaurant.jsx
+++ TacoTuesday/ClientApp/src/pages/Restaurant.jsx
@@ -14,21 +14,60 @@ export function Restaurant() {
     reviews: [],
   })
 
-  useEffect(() => {
-    async function fetchRestaurant() {
-      const response = await fetch(`/api/Restaurants/${id}`)
+  const [newReview, setNewReview] = useState({
+    body: '',
+    summary: '',
+    stars: 3,
+    restaurantId: id,
+  })
 
-      if (response.ok) {
-        const apiData = await response.json()
+  async function fetchRestaurant() {
+    const response = await fetch(`/api/Restaurants/${id}`)
 
-        setRestaurant(apiData)
-      }
+    if (response.ok) {
+      const apiData = await response.json()
+
+      setRestaurant(apiData)
     }
+  }
 
+  useEffect(() => {
     fetchRestaurant()
   }, [id])
 
-  console.log(restaurant.reviews)
+  function handleNewReviewTextFieldChange(event) {
+    const name = event.target.name
+    const value = event.target.value
+
+    setNewReview({ ...newReview, [name]: value })
+  }
+
+  function handleStarRadioButton(newStars) {
+    setNewReview({ ...newReview, stars: newStars })
+  }
+
+  async function handleNewReviewSubmit(event) {
+    event.preventDefault()
+
+    const response = await fetch(`/api/Reviews`, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(newReview),
+    })
+
+    if (response.ok) {
+      // Clear the form
+      setNewReview({
+        ...newReview,
+        body: '',
+        summary: '',
+        stars: 0,
+      })
+
+      // Reload the restaurant (including the reviews!)
+      fetchRestaurant()
+    }
+  }
 
   return (
     <>
@@ -92,10 +131,15 @@ export function Restaurant() {
           ))}
         </ul>
         <h3>Enter your own review</h3>
-        <form action="#">
+        <form onSubmit={handleNewReviewSubmit}>
           <p className="form-input">
             <label htmlFor="summary">Summary</label>
-            <input type="text" name="summary" />
+            <input
+              type="text"
+              name="summary"
+              value={newReview.summary}
+              onChange={handleNewReviewTextFieldChange}
+            />
             <span className="note">
               Enter a brief summary of your review. Example:{' '}
               <strong>Great food, good prices.</strong>
@@ -103,18 +147,57 @@ export function Restaurant() {
           </p>
           <p className="form-input">
             <label htmlFor="body">Review</label>
-            <textarea name="body"></textarea>
+            <textarea
+              name="body"
+              value={newReview.body}
+              onChange={handleNewReviewTextFieldChange}
+            ></textarea>
           </p>
           <div className="rating">
-            <input id="star-rating-1" type="radio" name="stars" value="1" />
+            <input
+              id="star-rating-1"
+              type="radio"
+              name="stars"
+              value="1"
+              checked={newReview.stars === 1}
+              onChange={() => handleStarRadioButton(1)}
+            />
             <label htmlFor="star-rating-1">1 star</label>
-            <input id="star-rating-2" type="radio" name="stars" value="2" />
+            <input
+              id="star-rating-2"
+              type="radio"
+              name="stars"
+              value="2"
+              checked={newReview.stars === 2}
+              onChange={() => handleStarRadioButton(2)}
+            />
             <label htmlFor="star-rating-2">2 stars</label>
-            <input id="star-rating-3" type="radio" name="stars" value="3" />
+            <input
+              id="star-rating-3"
+              type="radio"
+              name="stars"
+              value="3"
+              checked={newReview.stars === 3}
+              onChange={() => handleStarRadioButton(3)}
+            />
             <label htmlFor="star-rating-3">3 stars</label>
-            <input id="star-rating-4" type="radio" name="stars" value="4" />
+            <input
+              id="star-rating-4"
+              type="radio"
+              name="stars"
+              value="4"
+              checked={newReview.stars === 4}
+              onChange={() => handleStarRadioButton(4)}
+            />
             <label htmlFor="star-rating-4">4 stars</label>
-            <input id="star-rating-5" type="radio" name="stars" value="5" />
+            <input
+              id="star-rating-5"
+              type="radio"
+              name="stars"
+              value="5"
+              checked={newReview.stars === 5}
+              onChange={() => handleStarRadioButton(5)}
+            />
             <label htmlFor="star-rating-5">5 stars</label>
 
             <div className="star-rating">
