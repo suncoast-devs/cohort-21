19950ceb16b7a341905a0958a8af7ef008bab114
Gavin Stark <gavin@gstark.com>
2021-06-21 10:47:41 -0400

    Cleanup header and footer

--- TacoTuesday/ClientApp/src/pages/SignUp.jsx
+++ TacoTuesday/ClientApp/src/pages/SignUp.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
-import avatar from '../images/avatar.png'
 
 export function SignUp() {
   const history = useHistory()
@@ -40,69 +39,47 @@ export function SignUp() {
   }
 
   return (
-    <>
-      <header>
-        <ul>
-          <li>
-            <nav>
-              <a href="#">
-                <i className="fa fa-plus"></i> Restaurant
-              </a>
-              <p>Welcome back, Steve!</p>
-            </nav>
-          </li>
-          <li className="avatar">
-            <img src={avatar} alt="Steve's Avatar" height="64" width="64" />
-          </li>
-        </ul>
-      </header>
-      <main className="page">
-        <nav>
-          <a href="/">
-            <i className="fa fa-home"></i>
-          </a>
-          <h2>Sign Up</h2>
-        </nav>
+    <main className="page">
+      <nav>
+        <a href="/">
+          <i className="fa fa-home"></i>
+        </a>
+        <h2>Sign Up</h2>
+      </nav>
 
-        <form onSubmit={handleFormSubmit}>
-          {errorMessage ? <p>{errorMessage}</p> : null}
-          <p className="form-input">
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              name="fullName"
-              value={newUser.fullName}
-              onChange={handleStringFieldChange}
-            />
-          </p>
-          <p className="form-input">
-            <label htmlFor="name">Email</label>
-            <input
-              type="email"
-              name="email"
-              value={newUser.email}
-              onChange={handleStringFieldChange}
-            />
-          </p>
-          <p className="form-input">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              value={newUser.password}
-              onChange={handleStringFieldChange}
-            />
-          </p>
-          <p>
-            <input type="submit" value="Submit" />
-          </p>
-        </form>
-      </main>
-      <footer>
-        <p>
-          Built with <i className="fa fa-heart"></i> in St Petersburg, Florida.
+      <form onSubmit={handleFormSubmit}>
+        {errorMessage ? <p>{errorMessage}</p> : null}
+        <p className="form-input">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="fullName"
+            value={newUser.fullName}
+            onChange={handleStringFieldChange}
+          />
         </p>
-      </footer>
-    </>
+        <p className="form-input">
+          <label htmlFor="name">Email</label>
+          <input
+            type="email"
+            name="email"
+            value={newUser.email}
+            onChange={handleStringFieldChange}
+          />
+        </p>
+        <p className="form-input">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={newUser.password}
+            onChange={handleStringFieldChange}
+          />
+        </p>
+        <p>
+          <input type="submit" value="Submit" />
+        </p>
+      </form>
+    </main>
   )
 }
