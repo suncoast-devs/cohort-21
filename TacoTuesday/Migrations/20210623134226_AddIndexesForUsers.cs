0b51934e2f849b6a61025117f42a19e844280548
Gavin Stark <gavin@gstark.com>
2021-06-23 09:44:39 -0400

    Adds a migration for the Review to User relationship index and foreign keys

---
+++ TacoTuesday/Migrations/20210623134226_AddIndexesForUsers.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TacoTuesday.Migrations
+{
+    public partial class AddIndexesForUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_UserId",
+                table: "Reviews",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Users_UserId",
+                table: "Reviews",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Users_UserId",
+                table: "Reviews");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_UserId",
+                table: "Reviews");
+        }
+    }
+}
